24d2c542021cbb4b95b6856e48f57886cbbadfdc
ilker <ilker1525@hotmail.com>
2023-02-26 11:43:32 +0300

    Proje Katmanları Oluşturuldu - DbaApi - Kişi ve iletişim bilgileri yönerecek Api Katmanı - ReportApi - Raporlama yapacak olan katman - Test Klasörü içinde ki Projeler ile de Api ve Akış Testleri Yapılacak

---
+++ Test/Rise.PhoneBook.DbaApi.TestProject/MainUnitTest.cs
@@ -0,0 +1,11 @@
+namespace Rise.PhoneBook.DbaApi.TestProject
+{
+    public class MainUnitTest
+    {
+        [Fact]
+        public void MainTest()
+        {
+
+        }
+    }
+}
---
+++ Test/Rise.PhoneBook.ReportApi.TestProject/MainUnitTest.cs
@@ -0,0 +1,11 @@
+namespace Rise.PhoneBook.ReportApi.TestProject
+{
+    public class MainUnitTest
+    {
+        [Fact]
+        public void MainTest()
+        {
+
+        }
+    }
+}
